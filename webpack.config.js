c9ff0178c2b0ae212d5573d25fd5bb1195637ba1
liuyuqin <liuyuqin@jd.com>
2017-12-22 14:14:54 +0800

    demo2：实现在demo1的基础上添加CSS样式

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,19 @@ module.exports = {
 					loader: "babel-loader"
 				},
 				exclude: /node_modules/
+			},{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "style-loader"
+					}, {
+						loader: "css-loader",
+						options: {
+							modules: true, // 指定启用css modules
+							localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+						}
+					}
+				]
 			}
 		]
 	}
