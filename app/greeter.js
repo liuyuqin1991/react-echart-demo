fd8604186e140bfc4f5cf276c8fe5090c0146716
liuyuqin <liuyuqin@jd.com>
2017-12-22 11:25:41 +0800

    story：例子工程初始化

---
+++ app/greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by liuyuqin on 2017/12/22.
+ */
+module.exports = function() {
+	const greet = document.createElement('div');
+	greet.textContent = "Hi there and greetings!";
+	return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by liuyuqin on 2017/12/22.
+ */
+const greeter = require('./greeter.js');
+
+
+
+document.querySelector("#root").appendChild(greeter());
