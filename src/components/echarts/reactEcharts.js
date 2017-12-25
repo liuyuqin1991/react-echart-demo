846b4424deb27dee3a1dbe0d6b831386b0db3bdd
liuyuqin <liuyuqin@jd.com>
2017-12-25 16:24:23 +0800

    demo4：加入Echarts通用配置工厂，并使用

--- src/components/echarts/reactEcharts.js
+++ src/components/echarts/reactEcharts.js
@@ -5,28 +5,17 @@
 import React, {Component} from 'react'
 import styles from '../../css/echarts.css';
 import echarts from 'echarts';
+import { EchartRender } from './config'
 class ReactEcharts extends Component{
 	
 	
 	componentDidMount() {
 		// 基于准备好的dom，初始化echarts实例
 		const myChart = echarts.init(document.getElementById('main'));
-		// 绘制图表
-		myChart.setOption({
-			title: { text: 'ECharts 入门示例' },
-			tooltip: {},
-			xAxis: {
-				data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-			},
-			yAxis: {},
-			series: [{
-				name: '销量',
-				type: 'bar',
-				data: [5, 20, 36, 10, 10, 20]
-			}]
-		});
+		// 使用通用配置工厂进行echart配置
+		let option = EchartRender.initDefaultLine('ECharts 入门示例',["销量"],[[5, 20, 36, 10, 10, 20]],null,["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]);
+		myChart.setOption(option);
 	}
-	
 	
 	render() {
 		return (
